88099a123aa3ff72d4bc56f9a2812d0c009cb1c6
Soumitra <projwebinfo@gmail.com>
2023-11-15 15:35:50 +0530

    Update WebSocketConfig.java

--- spring-apache-kafka-streams-websocket-stomp/spring-apache-kafka-streams-websocket-stomp-server/src/main/java/com/roytuts/spring/apache/kafka/streams/websocket/stomp/server/config/WebSocketConfig.java
+++ spring-apache-kafka-streams-websocket-stomp/spring-apache-kafka-streams-websocket-stomp-server/src/main/java/com/roytuts/spring/apache/kafka/streams/websocket/stomp/server/config/WebSocketConfig.java
@@ -12,7 +12,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/websocket").setAllowedOrigins("*").withSockJS();
+		//registry.addEndpoint("/websocket").setAllowedOrigins("*").withSockJS();
+		registry.addEndpoint("/websocket").setAllowedOrigins("http://localhost:4200").withSockJS();
 	}
 
 	@Override
