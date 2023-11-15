da76bb150f4de77ea439c1aac7410dc4c24fb907
Soumitra <projwebinfo@gmail.com>
2023-11-15 15:35:22 +0530

    Update SpringKafkaStreamaRealTimeApp.java

--- spring-apache-kafka-streams-websocket-stomp/spring-apache-kafka-streams-websocket-stomp-server/src/main/java/com/roytuts/spring/apache/kafka/streams/websocket/stomp/server/SpringKafkaStreamaRealTimeApp.java
+++ spring-apache-kafka-streams-websocket-stomp/spring-apache-kafka-streams-websocket-stomp-server/src/main/java/com/roytuts/spring/apache/kafka/streams/websocket/stomp/server/SpringKafkaStreamaRealTimeApp.java
@@ -3,7 +3,7 @@ package com.roytuts.spring.apache.kafka.streams.websocket.stomp.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.roytuts.spring.apache.kafka.streams.websocket.stomp.server")
+@SpringBootApplication
 public class SpringKafkaStreamaRealTimeApp {
 
 	public static void main(String[] args) {
